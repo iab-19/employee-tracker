65f803ffdc6e757f34194c177e15d6589bc18c08
Ibrahima <iab.jr10@gmail.com>
2023-08-15 23:47:01 -0400

    Change data that is added to the database

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
-INSERT INTO department (name) VALUES ('Department1');
+INSERT INTO department (name) VALUES ('Management');
 
 INSERT INTO role (title, salary, department_id) VALUES ('Manager', 500, 1);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Bob', 'Smith', 1, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Nikola', 'Tesla', 1, 1);
