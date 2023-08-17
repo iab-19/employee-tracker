8fe1367f029e1ccad2f80abaa2160ebe7c108c04
Ibrahima <iab.jr10@gmail.com>
2023-08-17 01:51:59 -0400

    Change data in seeds.sql file

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,4 +2,4 @@ INSERT INTO department (name) VALUES ('Management');
 
 INSERT INTO role (title, salary, department_id) VALUES ('Manager', 500, 1);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Nikola', 'Tesla', 1, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Nikola', 'Tesla', 1, NULL);
